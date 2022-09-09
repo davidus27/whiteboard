550ea68c9a15376db009a18c9157cfa284c0cb56
davidus27 <davidisbest27@gmail.com>
2022-09-09 12:58:56 +0200

    remove console.logs and formatting

--- src/components/Emoji.svelte
+++ src/components/Emoji.svelte
@@ -7,47 +7,44 @@
     let { name, text } = stickerData;
 
     const getNumber = (size) => {
-        return Number(size.replace(/[^\d\.\-]/g, ''));
-    }
+        return Number(size.replace(/[^\d\.\-]/g, ""));
+    };
 
     const calculateFontSize = (resolution) => {
         const width = getNumber(resolution.width);
         const height = getNumber(resolution.height);
-        
+
         const width_2 = Math.pow(width, 2);
         const height_2 = Math.pow(height, 2);
 
-        return Math.sqrt(width_2 + height_2) / 2; 
-    }
-    
+        return Math.sqrt(width_2 + height_2) / 2;
+    };
 
     // props for the meta sticker
-    let { id, position, color, resolution, type } = stickerData;
-    const metaProps = { id, position, color, resolution, type };
+    let { id, position, resolution, type } = stickerData;
+    const metaProps = { id, position, resolution, type };
 
     let fontSize = calculateFontSize(resolution) + "px";
-    console.log(fontSize);
 
     export let updateLines;
     const update = () => {
         updateLines();
         fontSize = calculateFontSize(resolution) + "px";
-        console.log(resolution, fontSize);
-    } 
+    };
 </script>
 
-
-<MetaSticker {...metaProps} updateLines={update} { cancelSticker }>
-    <div class="emoji" style="font-size: { fontSize }">
-    { type }
+<MetaSticker {...metaProps} updateLines={update} {cancelSticker}>
+    <div class="emoji" style="font-size: {fontSize}">
+        {type}
     </div>
+    <!-- <input id="name-input" placeholder="TEST" /> -->
 </MetaSticker>
 
 <style>
-.emoji {
-    /* align it to center */
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
+    .emoji {
+        /* align it to center */
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 </style>
