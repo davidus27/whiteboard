f44130ac247d097453a7d1f899f7d7af4cb7a416
David Drobny <drobnda1@cvut.cz>
2023-04-27 00:20:43 +0200

    add wrapper utility functions
    This is for Konva integration with Svelte

---
+++ src/utils/wrapperUtils.ts
@@ -0,0 +1,47 @@
+export const stageKey = Symbol();
+export const parentKey = Symbol();
+export const layerKey = Symbol();
+
+export const eventNames = [
+    "mouseover",
+    "mouseout",
+    "mouseenter",
+    "mouseleave",
+    "mousemove",
+    "mousedown",
+    "mouseup",
+    "wheel",
+    "click",
+    "dblclick",
+    "touchstart",
+    "touchmove",
+    "touchend",
+    "tap",
+    "dbltap",
+    "pointerdown",
+    "pointermove",
+    "pointereup",
+    "pointercancel",
+    "pointerover",
+    "pointerenter",
+    "pointerout",
+    "pointerleave",
+    "pointerclick",
+    "pointerdblclick",
+    "dragstart",
+    "dragmove",
+    "dragend",
+    "transformstart",
+    "transform",
+    "transformend",
+];
+
+export function excludeKeys(object: any, disallow_list: string[]) {
+    let answer = {};
+    const filtered = Object.keys(object)
+        .filter((key) => !disallow_list.includes(key))
+        .map((key) => {
+            answer[key] = object[key];
+        });
+    return answer;
+}
